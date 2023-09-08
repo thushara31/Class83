25e922314323fad94c5dc6f15c08cbf792e0fff0
thushara31 <128104176+thushara31@users.noreply.github.com>
2023-09-07 20:28:17 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -27,32 +27,7 @@ var last_position_of_x, last_position_of_y;
   last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
   last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
     }
-
-    canvas.addEventListener("mousemove", my_mousemove);
-    function my_mousemove(e)
-    {
-
-         current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-         current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-
-        if (mouseEvent == "mouseDown") {
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-        ctx.stroke();
-        }
-
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
-    }
+    canvas.addEventListener("touchmove", my_touchmove);
     function clearArea() {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     }
